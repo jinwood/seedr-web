a425085584649f9775abd01b2e60d7e169135a3b
jinwood <jinw@protonmail.com>
2020-12-20 12:42:26 +0000

    Styling stuff

--- src/components/HomepageForm/layout.js
+++ src/components/HomepageForm/layout.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div>
       <header></header>
       <div className="global-wrapper">{children}</div>;<footer></footer>
-    </>
+      <div className="left-wrapper"></div>
+      <div className="right-wrapper"></div>
+    </div>
   );
 };
 
