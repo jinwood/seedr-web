22a2302d49f5ff8868a8e3a80fe86e7092b29b28
jinwood <jinw@protonmail.com>
2021-02-13 20:54:44 +0000

    Move functions out

--- functions/reddit/index.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const functions = require("firebase-functions");
-
-exports.onNewSeed = functions.firestore
-  .document("seeds")
-  .onWrite((change, context) => {
-    // interact with reddit
-  });
